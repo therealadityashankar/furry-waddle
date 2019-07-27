e1e097da9963bd5b38154ef3363ca83f00d4221c
Aditya Shankar <aditniru@gmail.com>
2019-07-28 01:04:57 +0530

    renamed some variables

--- test/nameTester.js
+++ test/nameTester.js
@@ -25,25 +25,25 @@ describe('nameParser', function(){
         var test3 = parseName("abcd[fgh]['def'][ijk]");
         checkValues(test3, ['abcd', 'fgh', 'def', 'ijk']);
 
-        var test3_5 = parseName("[abcd][fgh]['def'][ijk]");
+        var test4 = parseName("[abcd][fgh]['def'][ijk]");
         checkValues(test3_5, ['abcd', 'fgh', 'def', 'ijk']);
 
-        var test4 = parseName("abcd.fgh");
+        var test5 = parseName("abcd.fgh");
         checkValues(test4, ['abcd', 'fgh']);
 
-        var test5 = parseName("abcd.fgh['def'][ijk]");
+        var test6 = parseName("abcd.fgh['def'][ijk]");
         checkValues(test5, ['abcd', 'fgh', 'def', 'ijk']);
 
-        var test6 = parseName("abcd.efg[hij][3]");
+        var test7 = parseName("abcd.efg[hij][3]");
         checkValues(test6, ['abcd', 'efg', 'hij', 3]);
 
-        var test7 = () => parseName("abcd.efg[hij");
+        var test8 = () => parseName("abcd.efg[hij");
         assert.throws(test7, fw.BadNameError);
 
-        var test8 = () => parseName("abcd.efg[\"hij]");
+        var test9 = () => parseName("abcd.efg[\"hij]");
         assert.throws(test7, fw.BadNameError);
 
-        var test9 = () => parseName("abc'd.efg[\"hij]");
+        var test10 = () => parseName("abc'd.efg[\"hij]");
         assert.throws(test8, fw.BadNameError);
     });
 });
